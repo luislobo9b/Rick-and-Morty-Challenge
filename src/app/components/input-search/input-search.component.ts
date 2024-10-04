48100882406f98a4b1d1b6e3b2df5de1c76638de
luislobo9b <luis.lobo9b@gmail.com>
2024-10-03 21:57:21 -0300

    atualizado componente InputSearch com estrutura inicial (scss, eventos, observable, binding e autocomplete)

--- src/app/components/input-search/input-search.component.ts
+++ src/app/components/input-search/input-search.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, catchError, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-input-search',
   templateUrl: './input-search.component.html',
   styleUrls: ['./input-search.component.scss']
 })
-export class InputSearchComponent {
+export class InputSearchComponent implements OnInit {
+  searchControl = new FormControl()
+  inputName = ""
+  showAutocomplete = true
+  names = [
+  	"Rick Sanchez",
+  	"Morty Smith",
+  	"Summer Smith",
+  	"Beth Smith",
+  	"Jerry Smith",
+  	"Abadango Cluster Princess",
+  	"Abradolf Lincler",
+  	"Adjudicator Rick",
+  	"Agency Director",
+  	"Alan Rails",
+  	"Albert Einstein",
+  	"Alexander",
+  	"Alien Googah",
+  	"Alien Morty",
+  	"Alien Rick",
+  	"Amish Cyborg",
+  	"Annie",
+  	"Antenna Morty",
+  	"Antenna Rick",
+  	"Ants in my Eyes Johnson"
+  ].sort()
 
+  filteredNames!: Observable<string[]>
+
+  filterNames(value: string) {
+    const filterName = value.toLowerCase()
+
+    if (filterName === "") {
+      return []
+    }
+
+    return this.names.filter(name => {
+        const nameLowerCase = name.toLocaleLowerCase()
+        return nameLowerCase.startsWith(filterName) && nameLowerCase !== filterName
+      })
+  }
+
+  handleInput(): void {
+    this.showAutocomplete = true
+  }
+
+  handleAutocomplete(value: string): void {
+    this.inputName = value
+    this.showAutocomplete = false
+  }
+
+  ngOnInit(): void {
+    this.filteredNames = this.searchControl.valueChanges.pipe(
+      map(name => this.filterNames(name)),
+      catchError(error => {
+        console.error(error)
+        return of([])
+      })
+    )
+  }
 }
