c5e595b44ee01f5a69f2ab8555ca1c144b653055
luislobo9b <luis.lobo9b@gmail.com>
2024-10-09 14:38:27 -0300

    atualizado componente Navbar com número de favoritos dinâmico e criado ciclo de vida para isso

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Observable, Subscription } from 'rxjs'
+
+// ngxs
+import { Select, Store } from '@ngxs/store'
+import { FavoritesState } from '../../states/favorites/favorites.state'
+
+import { ICharacter } from '../../interfaces/IRickAndMortyApi'
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy, OnInit {
+  @Select(FavoritesState.getFavorites)
+  favorites$!: Observable<ICharacter[]>
+  favoritedCharacters!: ICharacter[]
 
+  favoritesSubscription!: Subscription
+
+  constructor(store: Store) { }
+
+  ngOnInit(): void {
+    this.favoritesSubscription = this.favorites$.subscribe((favorites:ICharacter[]) => {
+      this.favoritedCharacters = favorites
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.favoritesSubscription.unsubscribe()
+  }
 }
